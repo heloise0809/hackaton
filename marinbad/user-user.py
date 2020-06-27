5ed9ba04791291a7a2f8355f8262441797e4b621
heloise0809 <heloise.ferber@laplateforme.io>
2020-06-27 15:23:29 +0200

    Enlever des allumettes

--- marinbad/user-user.py
+++ marinbad/user-user.py
@@ -10,13 +10,37 @@ if __name__ == '__main__':
     for i in range(0, len(pyramide)):
         print pyramide[i]
 
-    ligne = input("Entrez le numero de ligne ")
-    n = input("Entrez le nombre d'allumettes a supprimer: ")
+    while(len(pyramide) != 0):
+        ligne = input("Entrez le numero de ligne ")
+        n = input("Entrez le nombre d'allumettes a supprimer: ")
 
-    for i in range(0, n):
-        if i==len(pyramide[ligne]):
-            pyramide=np.delete(pyramide, ligne, axis=0)
-            break
-        pyramide[ligne][i]=0
 
-    print pyramide
+        if n >= len(pyramide[ligne]):
+            pyramide = np.delete(pyramide, ligne, axis=0)
+            print pyramide
+            continue
+
+        # position du premier 1 a droite sur la ligne
+        i=len(pyramide[ligne])-1
+        while i>0:
+            while pyramide[ligne][i]==0:
+                i = i-1
+                continue
+            nbre_allumettes=i+1
+            #si nombre d'allumettes restants > nbre allumettes a supprimer
+            if n >= nbre_allumettes:
+                pyramide = np.delete(pyramide, ligne, axis=0)
+                print pyramide
+                break
+            #si la derniere allumettes va etre supprime
+            if pyramide[ligne][i]==1 and i==0:
+                pyramide = np.delete(pyramide, ligne, axis=0)
+                print pyramide
+                break
+
+            pyramide[ligne][i]=0
+            i=i-1
+            n=n-1
+
+
+            print pyramide
