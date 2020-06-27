f8e531c16a0db68f9c325c7c51b75ce4a9239c1f
heloise0809 <heloise.ferber@laplateforme.io>
2020-06-27 13:28:34 +0200

    premier code

--- marinbad/user-user.py
+++ marinbad/user-user.py
@@ -1,2 +1,11 @@
+import numpy as np
+#def allumettes(nbre, etages):
+
+
 if __name__ == '__main__':
-    print "coucou"
+    #n=input("Entrez le nombre d'allumettes")
+    #etages = input("Entrez le nombre d'allumettes")
+
+    pyramide = np.array([[1], [1, 1], [1, 1, 1]])
+    for i in range(0, len(pyramide)):
+        print pyramide[i]
