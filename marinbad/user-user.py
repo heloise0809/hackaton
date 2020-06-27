a8606d1af21ca585e96fdf8366e688e6e8dbac5e
heloise0809 <heloise.ferber@laplateforme.io>
2020-06-27 14:31:31 +0200

    Enlever une allumette

--- marinbad/user-user.py
+++ marinbad/user-user.py
@@ -9,3 +9,14 @@ if __name__ == '__main__':
     pyramide = np.array([[1], [1, 1], [1, 1, 1]])
     for i in range(0, len(pyramide)):
         print pyramide[i]
+
+    ligne = input("Entrez le numero de ligne ")
+    n = input("Entrez le nombre d'allumettes a supprimer: ")
+
+    for i in range(0, n):
+        if i==len(pyramide[ligne]):
+            pyramide=np.delete(pyramide, ligne, axis=0)
+            break
+        pyramide[ligne][i]=0
+
+    print pyramide
